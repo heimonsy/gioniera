74c130168f5c175cebc2b18b1dd053938d901e7b
heimonsy <heimonsy@gmail.com>
2016-07-21 15:52:40 +0800

    version two

--- src/component/Index.jsx
+++ src/component/Index.jsx
@@ -17,19 +17,38 @@ while (true) {
 var Index = React.createClass({
     handleClick(e) {
         e.preventDefault();
+
         if (this.state.text === "Love Enable") {
             this.setState(this.getInitialState());
         } else {
-            this.setState({text: "Love Enable", className: "btn btn-danger btn-lg", display: "block"});
+            var f = function(i, func) {
+                return function() {
+                    var state = this.getInitialState();
+                    state.processDisplay = "block";
+                    state.process = i + "%";
+                    this.setState(state);
+                    if(i == 100){
+                        return;
+                    }
+                    window.setTimeout(func(i+1, func), 30);
+                }.bind(this);
+            }.bind(this);
+            f(0, f)();
         }
     },
     componentDidMount() {
         hljs.initHighlightingOnLoad();
     },
     getInitialState() {
-        return {className: "btn btn-default btn-lg", text: "Love Disable", display: "none"};
+        return {className: "btn btn-default btn-lg", text: "Love Disable", processDisplay: "none", codesDisplay: "none", process: "0%"};
     },
     render() {
+        if (this.state.process === "100%") {
+            window.setTimeout(function() {
+                console.log(this.state.process);
+                this.setState({text: "Love Enable", className: "btn btn-danger btn-lg", processDisplay: "none", codesDisplay: "block", process: "0%"});
+            }.bind(this), 1000);
+        }
         return(
             <div className="center-box" style={{width: 600}}>
                 <div style={{textAlign: "center"}} className="row">
@@ -37,8 +56,14 @@ var Index = React.createClass({
                         {this.state.text}
                     </button>
                 </div>
+                <div className="row" style={{display: this.state.processDisplay}}>
+                    <br />
+                    <div className="progress progress-striped active">
+                        <div className="progress-bar progress-bar-danger" style={{width: this.state.process}}></div>
+                    </div>
+                </div>
                 <br />
-                <div style={{display: this.state.display}} className="row">
+                <div style={{display: this.state.codesDisplay}} className="row">
                     <pre><code className="java">{codes}</code></pre>
                 </div>
             </div>
